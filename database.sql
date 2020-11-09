426b43fd0a49e2066a0ff9ccbedd61bdef09c4f4
Dorian <dorian.naaji@gmail.com>
2020-11-09 11:09:46 +0100

    script SQL

---
+++ database.sql
@@ -0,0 +1,20 @@
+-- Projet Cloud --
+DROP DATABASE IF EXISTS `cloud-equipe-e`;
+CREATE DATABASE `cloud-equipe-e` DEFAULT CHARACTER SET utf8;
+USE `cloud-equipe-e`;
+
+
+CREATE TABLE `position`(
+`idPosition` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+`lat` DOUBLE NOT NULL,
+`lon` DOUBLE NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+CREATE TABLE `user`(
+`id` VARCHAR(255) PRIMARY KEY NOT NULL,
+`firstName` VARCHAR(100) NOT NULL,
+`lastName` VARCHAR(100) NOT NULL,
+`birthDay` DATE NOT NULL,
+`fkPosition` INT NOT NULL,
+CONSTRAINT `fkPositionConstraint` FOREIGN KEY(`fkPosition`) REFERENCES `position`(`idPosition`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
