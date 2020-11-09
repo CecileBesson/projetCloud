85688696338f4c30a96d9bddc2b0594772ca6bfe
Dorian <dorian.naaji@gmail.com>
2020-11-09 14:43:09 +0100

    Merge conflict

--- src/main/java/com/polytech/cloud/service/interfaces/IUserService.java
+++ src/main/java/com/polytech/cloud/service/interfaces/IUserService.java
@@ -1,14 +1,16 @@
 package com.polytech.cloud.service.interfaces;
 
 import com.polytech.cloud.entities.UserEntity;
+import com.polytech.cloud.exceptions.IncorrectlyFormedUserException;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface IUserService {
 
     public List<UserEntity> findAllUsers();
 
-    public serEntity findByIdUser(String id);
+    public UserEntity findByIdUser(String id);
     
-    public void saveAllRandomUsersToDatabase() throws IOException, IncorrectlyFormedUserException;
+    public void saveAllRandomUsersToDatabase() throws IOException, IncorrectlyFormedUserException, IOException, IncorrectlyFormedUserException;
 }
